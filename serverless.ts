fdefb7b5f1c72fd54c8410ef49f7479fca77e3f4
Tariq Elmughrabi <50697784+TuBl@users.noreply.github.com>
2023-08-02 16:48:21 -0400

    configured cors on api endpoints

--- serverless.ts
+++ serverless.ts
@@ -10,10 +10,12 @@ const serverlessConfiguration: AWS = {
     "serverless-esbuild",
     "serverless-dotenv-plugin",
     "serverless-offline",
+    "serverless-domain-manager",
   ],
   provider: {
     name: "aws",
     runtime: "nodejs18.x",
+    deploymentMethod: "direct",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
@@ -24,7 +26,32 @@ const serverlessConfiguration: AWS = {
     },
   },
   // import the function via paths
-  functions: { completions, ImageGen },
+  functions: {
+    completions: {
+      ...completions,
+      events: [
+        {
+          http: {
+            path: "completions",
+            method: "post",
+            cors: true,
+          },
+        },
+      ],
+    },
+    ImageGen: {
+      ...ImageGen,
+      events: [
+        {
+          http: {
+            path: "ImageGen",
+            method: "post",
+            cors: true,
+          },
+        },
+      ],
+    }
+  },
   package: { individually: true },
   custom: {
     esbuild: {
@@ -36,6 +63,14 @@ const serverlessConfiguration: AWS = {
       define: { "require.resolve": undefined },
       platform: "node",
       concurrency: 10,
+    },
+    customDomain: {
+      domainName: "api.mosaedy.com",
+      certificateName: "*.mosaedy.com",
+      basePath: "",
+      stage: "dev",
+      createRoute53Record: true,
+      apiType: "rest",
     },
   },
 };
